b5dec4e062bbddf551ea0169bea9d908d9e6432b
Adrien Pessu <7055334+adrienpessu@users.noreply.github.com>
2024-03-06 10:12:55 +0100

    Add .gitignore file and update server code

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    fs.readFile('index.html', 'utf8', (err, data) => {
-        if (err) {
-            res.writeHead(500);
-            return res.end('Error loading index.html');
-        }
+app.use(express.static(path.join(__dirname, '.')));
 
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(data);
-    });
-});
-
-server.listen(3000, () => {
+app.listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
-});``
+});
